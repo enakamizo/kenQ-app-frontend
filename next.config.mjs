d41130b5620ffa4b5da5600f0d5bb6cf4a0ba7cc
enakamizo <eiko.nkmz@gmail.com>
2025-07-21 20:45:17 +0900

    next.config修正：新エンドポイント

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/projects-list',
-        destination: 'https://app-advanced3-1-cgghbjavdyhdbfeb.canadacentral-01.azurewebsites.net/projects-list',
+        destination: 'app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list',
       },
     ];
   },
