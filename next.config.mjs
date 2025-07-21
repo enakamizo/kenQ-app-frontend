444bf21812231ffaee840bd518f6da0b90c863db
enakamizo <166042638+enakamizo@users.noreply.github.com>
2025-07-21 21:11:03 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   output: 'standalone',
   // async rewrites() {
     // return [
-      {
+     // {
         // source: '/api/projects-list',
         // destination: 'https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list',
-      },
-    ];
-  },
+     // },
+   // ];
+  // },
 }
 
 export default nextConfig;
