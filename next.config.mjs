028d2cb5e4ef88c93b63c68dd326c05b74e4ee3d
enakamizo <eiko.nkmz@gmail.com>
2025-08-03 04:19:06 +0900

    バックエンドURLを環境変数に3

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,18 @@
 const nextConfig = {
   output: 'standalone',
   async rewrites() {
-    return [
+        return [
       {
         source: '/api/projects-list',
-        //destination: 'https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list',
-        destination: `${process.env.NEXT_PUBLIC_AZURE_API_URL}/projects-list`,
+        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list`,
+      },
+      {
+        source: '/api/ai-diagnosis',
+        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/ai-diagnosis`,
+      },
+      {
+        source: '/api/confirm-form',
+        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/confirm-form`,
       },
     ];
   },
