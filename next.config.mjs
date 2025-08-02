fed22361abb80d9b21b3a3735cba0913289d25c8
enakamizo <eiko.nkmz@gmail.com>
2025-08-02 22:44:48 +0900

    login:環境変数から絶対urlでアクセスに変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // async rewrites() {
-    // return [
-     // {
-        // source: '/api/projects-list',
-        // destination: 'https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list',
-     // },
-   // ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/projects-list',
+        destination: 'https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list',
+      },
+    ];
+  },
 }
 
 export default nextConfig;
