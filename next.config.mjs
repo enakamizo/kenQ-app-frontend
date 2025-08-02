4b65da99508f6c950f2ad24dd0eca5af2f942905
enakamizo <eiko.nkmz@gmail.com>
2025-08-03 05:00:55 +0900

    バックエンドURLを環境変数に4

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-        return [
-      {
-        source: '/api/projects-list',
-        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list`,
-      },
-      {
-        source: '/api/ai-diagnosis',
-        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/ai-diagnosis`,
-      },
-      {
-        source: '/api/confirm-form',
-        destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/confirm-form`,
-      },
-    ];
-  },
+  //async rewrites() {
+      //  return [
+      //{
+      //  source: '/api/projects-list',
+      //  destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/projects-list`,
+      //},
+      //{
+      //  source: '/api/ai-diagnosis',
+      //  destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/ai-diagnosis`,
+      //},
+      //{
+      // source: '/api/confirm-form',
+      //  destination: `https://app-kenq-1-azf7d4eje9cgaah2.canadacentral-01.azurewebsites.net/confirm-form`,
+      //},
+    //];
+  //},
 }
 
 export default nextConfig;
